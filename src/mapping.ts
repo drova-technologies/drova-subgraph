5fef42a25a1fb310dfe6aa4e9940b7ec20d1b7d8
Evgeny Tsvigun <utgarda@gmail.com>
2022-08-23 23:14:28 +0300

    HoursRedeemed event mapped

--- src/mapping.ts
+++ src/mapping.ts
@@ -7,27 +7,27 @@ import {
   RoleRevoked
 } from "../generated/LoyaltyProgramPaymentGateway/LoyaltyProgramPaymentGateway"
 import { ExampleEntity } from "../generated/schema"
+import { HoursRedeemedEntity } from "../generated/schema"
 
 export function handleHoursRedeemed(event: HoursRedeemed): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+
+  const id = event.transaction.hash.toHex() + "_HoursRedeemed"
+  let entity = HoursRedeemedEntity.load(id)
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity = new HoursRedeemedEntity(id)
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
 
   // Entity fields can be set based on event parameters
   entity.from = event.params.from
   entity.to = event.params.to
+  entity.commissionReceiver = event.params.commissionReceiver
+  entity.payment = event.params.payment
+  entity.commission = event.params.commission
 
   // Entities can be written to the store with `.save()`
   entity.save()
